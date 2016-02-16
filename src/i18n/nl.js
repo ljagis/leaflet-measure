285a86629b612c0aff348dbda77f470422f15e0b
Patric <patric.slagveer@gmail.com>
2016-02-16 13:11:38 +0100

    ATLAS-1112 - Localization

---
+++ src/i18n/nl.js
@@ -0,0 +1,33 @@
+// en.js
+// English i18n translations
+
+module.exports = {
+  'measure': 'Meten',
+  'measureDistancesAndAreas': 'Meten van afstanden en oppervlakten',
+  'createNewMeasurement': 'Start een nieuwe meting',
+  'startCreating': 'Start de meting door te klikken op de kaart.',
+  'finishMeasurement': 'Meting afronden',
+  'lastPoint': 'Laatste punt',
+  'area': 'Oppervlakte',
+  'perimeter': 'Perimeter',
+  'pointLocation': 'Punt Lokatie',
+  'areaMeasurement': 'Oppervlakte meting',
+  'linearMeasurement': 'Lijnmeting',
+  'pathDistance': 'Path distance',
+  'centerOnArea': 'Center on this area',
+  'centerOnLine': 'Center on this line',
+  'centerOnLocation': 'Center on this location',
+  'cancel': 'Annuleren',
+  'delete': 'Verwijderen',
+  'acres': 'Hectares',
+  'feet': 'Feet',
+  'kilometers': 'Kilometers',
+  'hectares': 'Hectares',
+  'meters': 'Meters',
+  'miles': 'Miles',
+  'sqfeet': 'Sq Feet',
+  'sqmeters': 'Vierkante meters',
+  'sqmiles': 'Sq Miles',
+  'decPoint': '.',
+  'thousandsSep': ','
+};
