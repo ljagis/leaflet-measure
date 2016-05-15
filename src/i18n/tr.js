4b430c9510635c106e78d8fb9d4b224adb592fce
ekiziltas <erryan1988@gmail.com>
2016-05-16 00:04:02 +0300

    Turkish localization

---
+++ src/i18n/tr.js
@@ -0,0 +1,33 @@
+// tr.js
+// Turkish i18n translations
+
+module.exports = {
+    'measure': 'Hesapla',
+    'measureDistancesAndAreas': 'Uzaklýk ve alan hesapla',
+    'createNewMeasurement': 'Yeni hesaplama',
+    'startCreating': 'Yeni nokta ekleyerek hesaplamaya baþla',
+    'finishMeasurement': 'Hesaplamayý bitir',
+    'lastPoint': 'Son nokta',
+    'area': 'Alan',
+    'perimeter': 'Çevre uzunluðu',
+    'pointLocation': 'Nokta yeri',
+    'areaMeasurement': 'Alan hesaplamasý',
+    'linearMeasurement': 'Doðrusal hesaplama',
+    'pathDistance': 'Yol uzunluðu',
+    'centerOnArea': 'Bu alana odaklan',
+    'centerOnLine': 'Bu doðtuya odaklan',
+    'centerOnLocation': 'Bu yere odaklan',
+    'cancel': 'Çýkýþ',
+    'delete': 'Sil',
+    'acres': 'Dönüm',
+    'feet': 'Feet',
+    'kilometers': 'Kilometre',
+    'hectares': 'Hektar',
+    'meters': 'Metre',
+    'miles': 'Mil',
+    'sqfeet': 'Feet kare',
+    'sqmeters': 'Metre kare',
+    'sqmiles': 'Mil kare',
+    'decPoint': '.',
+    'thousandsSep': ','
+};
