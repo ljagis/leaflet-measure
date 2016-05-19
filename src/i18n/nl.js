11dc708aada646bf7de7f2d4c72486b349d76d4c
Erik Nijland <e.n.nijland@gmail.com>
2016-05-19 11:36:06 +0200

    Minor spelling issue.

--- src/i18n/nl.js
+++ src/i18n/nl.js
@@ -3,7 +3,7 @@
 
 module.exports = {
   'measure': 'Meet',
-  'measureDistancesAndAreas': 'Meet afstanden en oppervlakken',
+  'measureDistancesAndAreas': 'Meet afstanden en oppervlakten',
   'createNewMeasurement': 'Maak een nieuwe meting',
   'startCreating': 'Begin een meting door punten toe te voegen aan de kaart',
   'finishMeasurement': 'Beëindig meting',
