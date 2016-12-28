e2cbb0db4cd4d05be60b07622806d4ebc51cb787
Stanisław Jankowski <stanislaw.jankowski@cgm.com>
2016-12-28 22:12:07 +0100

    Add polish translation

---
+++ src/i18n/pl.js
@@ -0,0 +1,33 @@
+// pl.js
+// Polish i18n translations
+
+module.exports = {
+  'measure': 'Pomiar',
+  'measureDistancesAndAreas': 'Pomiar odległości i powierzchni',
+  'createNewMeasurement': 'Utwórz nowy pomiar',
+  'startCreating': 'Rozpocznij tworzenie nowego pomiaru poprzez dodanie punktów na mapie',
+  'finishMeasurement': 'Zakończ pomiar',
+  'lastPoint': 'Ostatni punkt',
+  'area': 'Powierzchnia',
+  'perimeter': 'Obwód',
+  'pointLocation': 'Punkt lokalizacji',
+  'areaMeasurement': 'Pomiar powierzchni',
+  'linearMeasurement': 'Pomiar liniowy',
+  'pathDistance': 'Długość ścieżki',
+  'centerOnArea': 'Środek tego obszaru',
+  'centerOnLine': 'Środek tej linii',
+  'centerOnLocation': 'Środek w tej lokalizacji',
+  'cancel': 'Anuluj',
+  'delete': 'Skasuj',
+  'acres': 'akrów',
+  'feet': 'stóp',
+  'kilometers': 'kilometrów',
+  'hectares': 'hektarów',
+  'meters': 'metrów',
+  'miles': 'mil',
+  'sqfeet': 'stóp kwadratowych',
+  'sqmeters': 'metrów kwadratowych',
+  'sqmiles': 'mil kwadratowych',
+  'decPoint': ',',
+  'thousandsSep': '.'
+};
